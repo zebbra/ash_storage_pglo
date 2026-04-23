12faab07954bfa0066bbf2a728d5ffe4e7cc3455
Michael Bärtschi <michael.baertschi@zebbra.ch>
2026-04-23 14:36:24 +0200

    chore: move alias above @doc in Resource.Info

--- lib/ash_storage_pglo/resource/info.ex
+++ lib/ash_storage_pglo/resource/info.ex
@@ -3,11 +3,11 @@ defmodule AshStoragePGLO.Resource.Info do
   Introspection helpers for `AshStoragePGLO.Resource` DSL options.
   """
 
+  alias Spark.Dsl.Extension
+
   @doc """
   Returns the configured `bufsize` for the given resource, or the default of 1MB.
   """
-  alias Spark.Dsl.Extension
-
   @spec bufsize(Spark.Dsl.t() | module()) :: pos_integer()
   def bufsize(resource) do
     Extension.get_opt(resource, [:lo], :bufsize, 1_048_576)
