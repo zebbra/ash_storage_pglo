ec06f3e7f92d0902699848b6d892d6a70ca1f2a9
Michael Bärtschi <michael.baertschi@zebbra.ch>
2026-04-24 10:26:04 +0200

    fix: export bufsize locals_without_parens for dependent project formatters

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,9 @@
 [
   import_deps: [:ash_postgres, :ash, :ash_storage, :reactor, :ecto, :ecto_sql],
   locals_without_parens: [bufsize: 1],
+  export: [
+    locals_without_parens: [bufsize: 1]
+  ],
   subdirectories: ["priv/*/migrations"],
   plugins: [Spark.Formatter],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
